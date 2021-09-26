027ffebfc6a9ae0bb0bec01fbad754a4d8e06428
Mohsen Banan <form@mohsen.banan.1.byname.net>
2021-09-26 22:13:23 +0000

    Unnecessary parameters.

--- update/sw/icm/py/importUcfIcmG.py
+++ update/sw/icm/py/importUcfIcmG.py
@@ -4,5 +4,5 @@ from unisos import icm
 icm.unusedSuppressForEval(ucf.__file__)  # in case icm and ucf are not used
 
 G = icm.IcmGlobalContext()
-G.icmLibsAppend = __file__
-G.icmCmndsLibsAppend = __file__
+# G.icmLibsAppend = __file__
+# G.icmCmndsLibsAppend = __file__
