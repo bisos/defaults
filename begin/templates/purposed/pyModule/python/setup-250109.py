8ce72e14eebfdb57dbd2a72b195aaec2f1fab310
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-01-26 12:35:58 -0800

    Cleaned up.

--- begin/templates/purposed/pyModule/python/setup-250109.py
+++ begin/templates/purposed/pyModule/python/setup-250109.py
@@ -43,36 +43,38 @@ def longDescription():
 # b:py3:pypi:setup/version Arguments  :forSys t :forPyPi t :constant "666"
 ####+BEGIN: b:py3:pypi:setup/version :comment "Auto Detected"
 
-# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.37 -- forSysVersion=0.3 -- constant=NA
+# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.1 -- forSysVersion=0.1 -- constant=NA
 def pkgVersion():
-        return '0.3'
+        return '0.1'
 
 ####+END:
 
 # b:py3:pypi:setup/requires :extras ; :requirements "requirements.txt" (bring here requirements.txt)
 ####+BEGIN: b:py3:pypi:setup/requires :extras ()
 
-requires = [ 
+requires = [
 "blee",
-"blee.csPlayer",
+"blee.icmPlayer",
 "bisos",
 "bisos.b",
 "bisos.common",
+"bisos.graphviz",
 ]
 ####+END:
 
 # b:py3:pypi:setup/scripts :comment
 ####+BEGIN: b:py3:pypi:setup/scripts :comment ""
 
-scripts = [ 
-'bin/bannaInfo.cs',
+scripts = [
+'bin/exmpl-graphviz.cs',
+'bin/seedGraphviz.cs',
 ]
 ####+END:
 
 # b:py3:pypi:setup/dataFiles :comment
 ####+BEGIN: b:py3:pypi:setup/dataFiles :comment "Instead of ./MANIFEST.in or in pyproject.toml"
 
-data_files = [ 
+data_files = [
 (' ',  ['lh-agpl3-LICENSE.txt', '_description.org', 'README.rst']),
 ]
 ####+END:
@@ -81,7 +83,7 @@ data_files = [
 ####+BEGIN: b:py3:pypi:setup/funcArgs :comment "defaults to --auto--"
 
 setuptools.setup(
-    name=pkgName(),  # 'bisos.banna'
+    name=pkgName(),  # bisos.capability
     version=pkgVersion(),
     packages=setuptools.find_packages(),
     scripts=scripts,
@@ -92,9 +94,11 @@ setuptools.setup(
     author_email='libre@mohsen.1.banan.byname.net',
     maintainer='Mohsen Banan',
     maintainer_email='libre@mohsen.1.banan.byname.net',
+    url='http://www.by-star.net/PLPC/180047',
     license='AGPL',
     description=description(),
     long_description=longDescription(),
+    download_url='http://www.by-star.net/PLPC/180047',
     install_requires=requires,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
